f627207b560ed8b6bbcb5c9726a04101b5f459be
anumshka <anushka.mpathak17@gmail.com>
2021-07-31 12:30:14 +0530

    Square Class Added

---
+++ Assignment2/src/Square.java
@@ -0,0 +1,34 @@
+public class Square implements Shape2D {
+
+    double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() throws InvalidException {
+        if (side < 0) {
+            throw new InvalidException("Negative Inputs not allowed!!");
+        }
+        return side * side;
+    }
+
+    @Override
+    public double getPerimeter() throws InvalidException {
+        if (side < 0) {
+            throw new InvalidException("Negative Inputs not allowed!!");
+        }
+        return 4 * side;
+    }
+
+    @Override
+    public void print() throws InvalidException {
+        System.out.println("Perimeter:" + getPerimeter());
+        System.out.println("Area:" + getArea());
+    }
+}
