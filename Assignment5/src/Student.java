f0f93d2fb02eb00ae9f1ae790ecf52d14a288f9f
anumshka <anushka.mpathak17@gmail.com>
2021-08-02 02:59:29 +0530

    Student class modified Here,DOB is in String format.

--- Assignment5/src/Student.java
+++ Assignment5/src/Student.java
@@ -1,25 +1,18 @@
 import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Student implements Serializable {
     String firstName;
-    Date dateOfBirth;
+    String dateOfBirth;
     Address address;
 
     public Student(String firstName, String dateOfBirth, Address address) {
         this.firstName = firstName;
-        try {
-            this.dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        this.dateOfBirth = dateOfBirth;
         this.address = address;
     }
     //getter methods
 
-    public Date getDateOfBirth() {
+    public String getDateOfBirth() {
         return dateOfBirth;
     }
 
@@ -33,8 +26,8 @@ public class Student implements Serializable {
     //print
 
     public void print() {
-        String newstr = new SimpleDateFormat("MM/dd/yyyy").format(getDateOfBirth());
-        System.out.println("Name: " + getFirstName() + " DOB: " + newstr);
+
+        System.out.println("Name: " + getFirstName() + " DOB: " + getDateOfBirth());
         System.out.println(getAddress().print());
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
