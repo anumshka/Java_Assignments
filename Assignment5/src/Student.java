397928348a8386fd30903cbbb122913dbd3aa9a4
anumshka <anushka.mpathak17@gmail.com>
2021-08-02 02:35:07 +0530

    Modified Student class Added the object of address class and a function to print all the details.

--- Assignment5/src/Student.java
+++ Assignment5/src/Student.java
@@ -1,20 +1,48 @@
-public class Student
-{
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Student implements Serializable {
     String firstName;
-    String dateOfBirth;
+    Date dateOfBirth;
+    Address address;
 
-    Student(String firstName,String dateOfBirth)
-    {
+    public Student(String firstName, String dateOfBirth, Address address) {
         this.firstName = firstName;
-        this.dateOfBirth=dateOfBirth;
+        try {
+            this.dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        this.address = address;
     }
+    //getter methods
 
-    public String getDateOfBirth() {
+    public Date getDateOfBirth() {
         return dateOfBirth;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
+    public Address getAddress() {
+        return address;
+    }
+    //print
+
+    public void print() {
+        String newstr = new SimpleDateFormat("MM/dd/yyyy").format(getDateOfBirth());
+        System.out.println("Name: " + getFirstName() + " DOB: " + newstr);
+        System.out.println(getAddress().print());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+
 }
+
+
+
+
 
 
