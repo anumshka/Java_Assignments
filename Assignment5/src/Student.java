1c1a088ef4af2734a5616b256b3555e9feb8131e
anumshka <anushka.mpathak17@gmail.com>
2021-08-02 03:12:11 +0530

    Student class modified Here,the DOB is in date format

--- Assignment5/src/Student.java
+++ Assignment5/src/Student.java
@@ -1,18 +1,26 @@
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Student implements Serializable {
     String firstName;
-    String dateOfBirth;
+    Date dateOfBirth;
     Address address;
 
     public Student(String firstName, String dateOfBirth, Address address) {
         this.firstName = firstName;
-        this.dateOfBirth = dateOfBirth;
+        try {
+            this.dateOfBirth = new SimpleDateFormat("dd/MM/yyyy").parse(dateOfBirth);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         this.address = address;
     }
     //getter methods
 
-    public String getDateOfBirth() {
+    public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
@@ -26,8 +34,8 @@ public class Student implements Serializable {
     //print
 
     public void print() {
-
-        System.out.println("Name: " + getFirstName() + " DOB: " + getDateOfBirth());
+        String newstr = new SimpleDateFormat("MM/dd/yyyy").format(getDateOfBirth());
+        System.out.println("Name: " + getFirstName() + " DOB: " + newstr);
         System.out.println(getAddress().print());
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
